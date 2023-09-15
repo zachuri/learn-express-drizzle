0573c84b52ab3f2017f792d79696d868437505ca
zachuri <punsalang.zachary@gmail.com>
2023-09-15 03:47:43 -0700

    removed unnecessary code from index

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,3 @@
-// import express from 'express';
-// import {
-//   getAllCities,
-//   insertCountry,
-//   insertCity,
-//   getAllCountries,
-//   getAllCountriesWithCities,
-//   getAllCitiesWithCountry
-// } from './services/queries';
-
-// app.get('/cities', (req, res) => {
-//   getAllCities().then(allCities => {
-//     res.json(allCities);
-//   });
-// });
-
-// app.get('/citiesWithCountry', (req, res) => {
-//   getAllCitiesWithCountry().then(allCitiesWithCountry => {
-//     res.json(allCitiesWithCountry);
-//   });
-// });
-
-// // Please use POST for inserting data ;)
-// app.get('/insertCities', async (req, res) => {
-//   const cities = await insertCity([
-//     { id: 1, name: 'Berlin', countryId: 1, popularity: 'popular' },
-//     { id: 2, name: 'Jersey', countryId: 2, popularity: 'unknown' },
-//     { id: 3, name: 'London', countryId: 3, popularity: 'known' },
-//     { id: 4, name: 'Luton', countryId: 3, popularity: 'known' },
-//     { id: 5, name: 'Hamburg', countryId: 1, popularity: 'known' }
-//   ]);
-
-//   res.send(cities);
-// });
-
 import express, { Request, Response, NextFunction } from 'express';
 import countriesRoute from './routes/countries.route';
 import citiesRoute from './routes/cities.route';
